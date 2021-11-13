ede1fd198c262688c24bb19683d4af880c01abd0
yangguohao <70266361+yangguohao@users.noreply.github.com>
2021-11-13 19:42:59 +0800

    Create 测试文件.py

---
+++ documents/测试文件.py
@@ -0,0 +1,8 @@
+from paddle_quantum.state import vec_random,density_op_random
+from paddle_quantum.utils import plot_density_graph
+
+state = paddle.to_tensort(vec_random(2),dtype='float64')
+density_matrix = density_op_random(2)
+
+plot_density_graph(state)
+plot_density_graph(density_matrix)
