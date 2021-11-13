debc413e561464404ec0066dfe0c18d837585778
yangguohao <70266361+yangguohao@users.noreply.github.com>
2021-11-13 19:52:43 +0800

    Update 测试文件.py

--- documents/测试文件.py
+++ documents/测试文件.py
@@ -1,7 +1,8 @@
 from paddle_quantum.state import vec_random,density_op_random
 from paddle_quantum.utils import plot_density_graph
 
-state = paddle.to_tensort(vec_random(2),dtype='float64')
+
+state = paddle.to_tensor(vec_random(2),dtype='float64')
 density_matrix = density_op_random(2)
 
 plot_density_graph(state)
